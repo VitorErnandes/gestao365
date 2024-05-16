72358e1c79c6b13bef11786aca93ede2153cc299
vitor.oliveira40 <vitor.oliveira40@bitbucket.org>
2024-05-16 17:58:06 -0300

    add product controller and model with same functions

---
+++ app/Http/Controllers/Product/ProductController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Product
+{
+  public function index()
+  {
+    $products = Product::all();
+    return view('products.index', compact('products'));
+  }
+
+  public function create()
+  {
+    return view('products.create');
+  }
+
+  public function edit($id)
+  {
+    $product = Product::find($id);
+    return view('products.edit', compact('product'));
+  }
+}
