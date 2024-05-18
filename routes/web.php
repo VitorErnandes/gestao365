d35eb076b54bc727144d74a33f894bb6516dc380
VitorErnandes <vitor.contas40@gmail.com>
2024-05-18 03:46:50 -0300

    add the laravel/breeze to application to controle the access

--- routes/web.php
+++ routes/web.php
@@ -2,26 +2,29 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\User\UserController;
 use App\Http\Controllers\Permission\PermissionController;
 use App\Http\Controllers\Role\RoleController;
 
-//Permissions
-Route::resource('permissions', PermissionController::class);
-Route::delete('/permissions/{id}', [PermissionController::class, 'destroy'])->name('permissions.destroy');
+Route::group(['middleware' => ['role:super-admin|admin']], function () {
+    //Permissions
+    Route::resource('permissions', PermissionController::class);
+    Route::delete('/permissions/{id}', [PermissionController::class, 'destroy'])->name('permissions.destroy');
 
-// Roles
-Route::resource('roles', RoleController::class);
-Route::delete('/roles/{id}', [RoleController::class, 'destroy'])->name('roles.destroy');
-Route::get('/roles/{id}/give-permissions', [RoleController::class, 'addPermissionToRole'])->name('roles.addPermissionToRole');
-Route::put('/roles/{id}/give-permissions', [RoleController::class, 'givePermissionToRole'])->name('roles.givePermissionToRole');
+    // Roles
+    Route::resource('roles', RoleController::class);
+    Route::delete('/roles/{id}', [RoleController::class, 'destroy'])->name('roles.destroy');
+    Route::get('/roles/{id}/give-permissions', [RoleController::class, 'addPermissionToRole'])->name('roles.addPermissionToRole');
+    Route::put('/roles/{id}/give-permissions', [RoleController::class, 'givePermissionToRole'])->name('roles.givePermissionToRole');
 
-//Users
-Route::get('/users', [UserController::class, 'index'])->name('users.index');
-Route::post('/users/store', [UserController::class, 'store'])->name('users.store');
-Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
-Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
-Route::put('/users/{id}', [UserController::class, 'updateUser'])->name('users.update');
-Route::get('/users/{id}/editPassword', [UserController::class, 'editPassword'])->name('users.editPassword');
-Route::put('/users/editPassword/{id}', [UserController::class, 'updatePassword'])->name('users.updatePassword');
-Route::delete('/users/{id}', [UserController::class, 'destroyUser'])->name('users.destroy');
+    //Users
+    Route::get('/users', [UserController::class, 'index'])->name('users.index');
+    Route::post('/users/store', [UserController::class, 'store'])->name('users.store');
+    Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+    Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
+    Route::put('/users/{id}', [UserController::class, 'updateUser'])->name('users.update');
+    Route::get('/users/{id}/editPassword', [UserController::class, 'editPassword'])->name('users.editPassword');
+    Route::put('/users/editPassword/{id}', [UserController::class, 'updatePassword'])->name('users.updatePassword');
+    Route::delete('/users/{id}', [UserController::class, 'destroyUser'])->name('users.destroy');
+});
