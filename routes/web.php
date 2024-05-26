8c73dc819eafc8787a7c5be592bde6e04b7911d6
VitorErnandes <vitor.contas40@gmail.com>
2024-05-26 20:02:38 -0300

    adjust to redirect to login when the user is not authenticated

--- routes/web.php
+++ routes/web.php
@@ -10,27 +10,29 @@ use App\Http\Controllers\User\UserController;
 use App\Http\Controllers\Permission\PermissionController;
 use App\Http\Controllers\Role\RoleController;
 
-Route::group(['middleware' => ['role:super-admin|Usuário']], function () {
-    //Permissions
-    Route::resource('permissions', PermissionController::class);
+Route::group(['middleware' => ['auth']], function () {
+    Route::group(['middleware' => ['role:super-admin|Usuário']], function () {
+        //Permissions
+        Route::resource('permissions', PermissionController::class);
 
-    // Roles
-    Route::resource('roles', RoleController::class);
-    Route::get('/roles/{id}/give-permissions', [RoleController::class, 'addPermissionToRole'])->name('roles.addPermissionToRole');
-    Route::put('/roles/{id}/give-permissions', [RoleController::class, 'givePermissionToRole'])->name('roles.givePermissionToRole');
+        // Roles
+        Route::resource('roles', RoleController::class);
+        Route::get('/roles/{id}/give-permissions', [RoleController::class, 'addPermissionToRole'])->name('roles.addPermissionToRole');
+        Route::put('/roles/{id}/give-permissions', [RoleController::class, 'givePermissionToRole'])->name('roles.givePermissionToRole');
 
-    //Users
-    Route::resource('users', UserController::class);
+        //Users
+        Route::resource('users', UserController::class);
+    });
+
+    Route::get('/dashboard', [App\Http\Controllers\Global\GlobalController::class, 'dashboard'])
+        ->name('dashboard');
+
+    Route::get('/welcome', [App\Http\Controllers\Global\GlobalController::class, 'index'])
+        ->name('dashboard');
+
+    Route::get('/index', [App\Http\Controllers\Global\GlobalController::class, 'index'])
+        ->name('dashboard');
+
+    Route::get('/users/{id}/editPassword', [UserController::class, 'editPassword'])->name('users.editPassword')->middleware('permission:Alterar senha usuário');
+    Route::put('/users/editPassword/{id}', [UserController::class, 'updatePassword'])->name('users.updatePassword')->middleware('permission:Alterar senha usuário');
 });
-
-Route::get('/dashboard', [App\Http\Controllers\Global\GlobalController::class, 'dashboard'])
-    ->name('dashboard');
-
-Route::get('/welcome', [App\Http\Controllers\Global\GlobalController::class, 'index'])
-    ->name('dashboard');
-
-Route::get('/index', [App\Http\Controllers\Global\GlobalController::class, 'index'])
-    ->name('dashboard');
-
-Route::get('/users/{id}/editPassword', [UserController::class, 'editPassword'])->name('users.editPassword')->middleware('permission:Alterar senha usuário');
-Route::put('/users/editPassword/{id}', [UserController::class, 'updatePassword'])->name('users.updatePassword')->middleware('permission:Alterar senha usuário');
