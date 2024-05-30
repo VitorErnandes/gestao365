0b34411b2603f085b7b89ae01396867b26efff2a
VitorErnandes <vitor.contas40@gmail.com>
2024-05-29 23:56:52 -0300

    create the products group controller and model and organize the route to products

--- routes/products/web.php
+++ routes/products/web.php
@@ -3,13 +3,12 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Product\ProductController;
+use App\Http\Controllers\Product\ProductsGroupController;
 
-Route::group(['prefix' => 'products', 'namespace' => 'App\Http\Controllers\Product'], function () {
-    Route::get('/', [ProductController::class, 'index'])->name('products.index');
-    Route::get('/create', [ProductController::class, 'create'])->name('products.create');
-    Route::post('/', [ProductController::class, 'store'])->name('products.store');
-    Route::get('/{product}', [ProductController::class, 'show'])->name('products.show');
-    Route::get('/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
-    Route::put('/{product}', [ProductController::class, 'update'])->name('products.update');
-    Route::delete('/{product}', [ProductController::class, 'destroy'])->name('products.destroy');
+Route::prefix('products')->group(function () {
+    Route::resource('products', ProductController::class);
+});
+
+Route::prefix('product-groups')->group(function () {
+    Route::resource('products-group', ProductsGroupController::class);
 });
