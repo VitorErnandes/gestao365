562ea925fd6065f6a8e6f7b59d7483bd7620d771
VitorErnandes <vitor.contas40@gmail.com>
2024-05-29 23:45:02 -0300

    organizing my routes

---
+++ routes/products/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\Product\ProductController;
+
+Route::group(['prefix' => 'products', 'namespace' => 'App\Http\Controllers\Product'], function () {
+    Route::get('/', [ProductController::class, 'index'])->name('products.index');
+    Route::get('/create', [ProductController::class, 'create'])->name('products.create');
+    Route::post('/', [ProductController::class, 'store'])->name('products.store');
+    Route::get('/{product}', [ProductController::class, 'show'])->name('products.show');
+    Route::get('/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
+    Route::put('/{product}', [ProductController::class, 'update'])->name('products.update');
+    Route::delete('/{product}', [ProductController::class, 'destroy'])->name('products.destroy');
+});
--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,18 @@
 <?php
 
-require __DIR__ . '/auth.php';
-require __DIR__ . '/users/web.php';
-
 use Illuminate\Support\Facades\Route;
-
 use App\Http\Controllers\ProfileController;
-
 use App\Http\Controllers\Global\GlobalController;
 
-Route::group(['middleware' => ['auth']], function () {
-    Route::get('/dashboard', [GlobalController::class, 'dashboard'])
-        ->name('dashboard');
+require __DIR__ . '/auth.php';
+require __DIR__ . '/users/web.php';
+require __DIR__ . '/products/web.php';
 
-    Route::get('/welcome', [GlobalController::class, 'index'])
-        ->name('dashboard');
+Route::get('/dashboard', [GlobalController::class, 'dashboard'])
+    ->name('dashboard');
 
-    Route::get('/index', [GlobalController::class, 'index'])
-        ->name('dashboard');
-});
+Route::get('/welcome', [GlobalController::class, 'index'])
+    ->name('dashboard');
+
+Route::get('/index', [GlobalController::class, 'index'])
+    ->name('dashboard');
